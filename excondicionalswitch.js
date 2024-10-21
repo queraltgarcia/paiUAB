0938c221e75d235458c74aa0a969135a3f3aff6a
queraltgarcia <1706784@uab.cat>
2024-10-21 20:39:52 +0200

    Create excondicionalswitch.js

---
+++ excondicionalswitch.js
@@ -0,0 +1,45 @@
+//15.1. - Fent servir la instrucció switch escriu un programa que donat amb prompt un número de mes, entre 1 i 12, escrigui per pantalla el nom del mes que correspon
+/*let mes = parseInt(prompt("Introdueix un número (1-12)"));
+switch(mes){
+    case 1:
+        text="Gener";
+        break;
+    case 2:
+        text="Febrer";
+        break;
+    case 3:
+        text="Març";
+        break;
+    case 4:
+        text="Abril";
+        break;
+    case 5:
+        text="Maig";
+        break;
+    case 6:
+        text="Juny";
+        break;
+    case 7:
+        text="Juliol";
+        break;
+    case 8:
+        text="Agost"
+        break;
+    case 9:
+        text="Setembre";
+        break;
+    case 10:
+        text="Octubre";
+        break;
+    case 11:
+        text="Novembre";
+        break;
+    case 12:
+        text="Desembre";
+        break;
+    default:
+        text="Número no vàlid";
+}
+console.log(text);*/
+
+//15.2. - 
