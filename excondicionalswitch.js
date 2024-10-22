cbcc4f7d31dbfc72e789c66222c20e52355bda51
queraltgarcia <1706784@uab.cat>
2024-10-22 10:04:31 +0200

    Update excondicionalswitch.js

--- excondicionalswitch.js
+++ excondicionalswitch.js
@@ -42,4 +42,68 @@ switch(mes){
 }
 console.log(text);*/
 
-//15.2. - 
+//15.2. - Un usuari va a una gelateria i per calcular el cost del seu gelat té un ordinador on posa dues dades:
+//Si vol un conus (c) o una terrina (t). El conus té un cost base de 3.45 i la terrina de 3.95
+//Quin d’aquests sabors vol: Vainilla o Xocolata. No cal afegir res; Turró, Menta o Oreo. Caldrà afegir 0.5 al preu final.; Crema, Gerds o Ametlles. Caldrà afegir 1 al preu final.
+//Fes servir la instrucció switch per fer el càlcul de l’increment de preu associat al sabor.
+//Si l’usuari posa alguna dada malament, el missatge que es posarà a la consola serà “Error dades mal introduïdes”.
+//Si totes les dades són correctes, escriurà el preu final.
+/*let envas = prompt("Introdueix el envás en el que voldrás el gelat: Conus (c) o Terrina (t)");
+let sabor = prompt("Introdueix el gust de gelat que vols");
+if (envas=="c"){
+    switch(sabor){
+        case "Vainilla":
+        case "Xocolata":
+            text=3.45+"$";
+            break;
+        case "Turró":
+        case "Menta":
+        case "Oreo":
+            text=3.95+"$";
+            break;
+        case "Crema":
+        case "Gerds":
+        case "Ametlla":
+            text=4.45+"$";
+            break;
+        default:
+            text="Dades no valides";
+    }
+}else if(envas=="t"){
+    switch(sabor){
+        case "Vainilla":
+        case "Xocolata":
+            text=3.95+"$";
+            break;
+        case "Turró":
+        case "Menta":
+        case "Oreo":
+            text=4.45+"$";
+            break;
+        case "Crema":
+        case "Gerds":
+        case "Ametlla":
+            text=4.95+"$";
+            break;
+        default:
+            text="Dades no valides";
+    }
+}
+console.log(text);*/
+
+//15.3. - Escriu un programa, que donat una lletra que representa un dia de la setmana (l -dilluns, m - dimarts, x - dimecres, j - dijous, …) escrigui a la consola si el dia és laborable o festiu. Són laborables de dilluns a divendres. Fes servir la instrucció switch.
+/*let diaSetmana = prompt("Escriu el dia de la setmana: l-dilluns, m-dimarts, x-dimecres, j-dijous, v-divendres, s-dissabte, g-diumenge");
+switch(diaSetmana){
+    case "l":
+    case "m":
+    case "x":
+    case "j":
+    case "v":
+        text="Dia laborable";
+        break;
+    case "s":
+    case "g":
+        text="Dia festiu";
+        break;
+}
+console.log(text);*/
