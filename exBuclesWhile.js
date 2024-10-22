f86a9a055a7d58e25514a5c2c901b7e9a7e3a29b
queraltgarcia <1706784@uab.cat>
2024-10-22 12:33:22 +0200

    Create exBuclesWhile.js

---
+++ exBuclesWhile.js
@@ -0,0 +1,6 @@
+//17.1. - Escriu, fent servir la instrucció while, un programa que escrigui a la consola 10 cops el text “Estem fent bucles”
+/*let i=1;
+while (i<=10){
+    console.log("Estem fent bucles while.");
+    i+=1;
+}*/
