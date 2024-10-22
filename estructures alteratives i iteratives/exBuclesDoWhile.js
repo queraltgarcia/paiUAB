993bd0ddc513676a8ce108f82ccd672329d1c243
queraltgarcia <1706784@uab.cat>
2024-10-22 21:53:36 +0200

    Create exBuclesDoWhile.js

---
+++ estructures alteratives i iteratives/exBuclesDoWhile.js
@@ -0,0 +1,6 @@
+//18.1. - Escriu, fent servir la instrucció do-while, un programa que escrigui a la consola 10 cops el text “Estem fent bucles”. 
+let i=0;
+do{
+    console.log("Estem fent bucles");
+}
+while(i<=10);
