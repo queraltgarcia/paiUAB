69a193ae7a51a656237c4c4bb3e66cb5e787937d
queraltgarcia <1706784@uab.cat>
2024-10-24 13:33:18 +0200

    Update exBuclesDoWhile.js

--- estructures alteratives i iteratives/exBuclesDoWhile.js
+++ estructures alteratives i iteratives/exBuclesDoWhile.js
@@ -1,6 +1,42 @@
 //18.1. - Escriu, fent servir la instrucció do-while, un programa que escrigui a la consola 10 cops el text “Estem fent bucles”. 
-let i=0;
+/*let i=0;
 do{
     console.log("Estem fent bucles");
-}
-while(i<=10);
+    i++;
+}while(i<=10);*/
+
+//18.2. - Escriu, fent servir la instrucció for, un programa que escrigui a la consola tots els nombres del 5 al 20.
+/*let i = 5;
+do{
+    console.log(i);
+    i++;
+}while(i<=20);*/
+
+//18.3. - Escriure un programa, fent servir la instrucció do-while, que, donat un número N, escrigui a la consola tots els números de N a 1 (per tant, en ordre descendent).
+/*let N = parseInt(pompt("Introdueix un número"));
+let i = N;
+do{
+    console.log(i);
+    i--;
+}while(i>=1);*/
+
+//18.4. -  Escriu un programa que, donat un nombre N, ens digui si és primer.
+/*let N = parseInt(prompt("Introdueix un número"));
+do{
+    console.log("El número és primer");
+}*/
+
+//18.5. - Escriu, fent servir la instrucció do-while, un programa que demani (fent servir la instrucció prompt), 10 números, i ens digui quin és el més gran.
+/*let n1 = parseInt(prompt("Introdueix el primer número"));
+let n2 = parseInt(prompt("Introdueix el segon número"));
+let n3 = parseInt(prompt("Introdueix el tercer número"));
+let n4 = parseInt(prompt("Introdueix el quart número"));
+let n5 = parseInt(prompt("Introdueix el cinquè número"));
+let n6 = parseInt(prompt("Introdueix el sisè número"));
+let n7 = parseInt(prompt("Introdueix el setè número"));
+let n8 = parseInt(prompt("Introdueix el vuitè número"));
+let n9 = parseInt(prompt("Introdueix el novè número"));
+let n10 = parseInt(prompt("Introdueix el deuè número"));
+do{
+    if(n1>n2)
+}*/
