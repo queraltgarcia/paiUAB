a1a5bb6118767b468ca88aa2fb1acf94ad9732a8
queraltgarcia <1706784@uab.cat>
2024-11-08 16:45:51 +0100

    Create exArrays.js

---
+++ arrays/exArrays.js
@@ -0,0 +1,43 @@
+//21.1. - Fes un programa que faci el següent
+// Defineix una variable de tipus taula, buida. 
+// Afegeixi el valor “Hola” a la posició 0. 
+// Afegeixi el valor “Adeu” a la posició 1. 
+// Escrigui a la consola el contingut de la taula. 
+/*let array = [];
+array[0] = "Hola";
+array[1] = "Adeu";
+console.log(array);*/
+
+//21.2. - Al programa anterior, afegeix les instruccions necessàries:
+// Afegeixi el valor “Nom” a la posició 2 de la taula. 
+// Escrigui a la consola el contingut de la taula. 
+// Modifiqui el valor emmagatzemat a la posició 1 de la taula, guardant el valor “, “ 
+// Escrigui a la consola el contingut de la taula. 
+/*array[2] = "Nom";
+console.log(array);
+array[1] = ",";
+console.log(array);*/
+
+//21.3. - Fes un programa que faci el següent: 
+// Defineixi una variable de tipus taula, amb aquests elements [23,45,98,73]. 
+// Escrigui a la consola la suma dels elements situats a les posicions 0 i 2 de la taula. 
+// Guardi a la posició 6 de la taula la suma dels elements  
+// Escrigui a la consola el contingut de la taula. Què passa a la posició 5?
+/*let array = [];
+array[0] = 23;
+array[1] = 45;
+array[2] = 98;
+array[3] = 73;
+let suma = array[0] + array[2];
+console.log(suma);
+array[6] = suma;
+console.log(array);*/
+
+//21.4. -  Fes un programa que, donat un número (que demanarem per pantalla fent servir prompt()) emmagatzemi a una taula el doble i el triple d’aquest número. Un cop fet, escriu la taula a la consola.
+/*let num = prompt("Introdueix un número: ");
+let doble = num*2;
+let triple = num*3;
+let array = [];
+array[0] = doble;
+array[1] = triple;
+console.log(array);*/
