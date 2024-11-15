9bc201c67e7ee67854545f629506d1304b287bf6
queraltgarcia <1706784@uab.cat>
2024-11-15 15:39:46 +0100

    Create objectes.js

---
+++ objectes/objectes.js
@@ -0,0 +1,68 @@
+//Exemple guiat:
+/*class Empresa{
+    constructor (nom, adreca){
+        // Propietats
+        this.nom = nom;
+        this.adreca = adreca;
+        this.deute = 0; // Quan creem l'empresa té dute 0.
+    }
+    //Mètode
+    incrementaDeute(n){
+        this.deute += n;
+    }
+    //Mètode
+    totPagat(){
+        yhis.deute = 0;
+    }
+}
+class Paquet{
+    constructor (origen, destinacio, cost){
+        //Propietats
+        this.origen = origen;
+        this.destinacio = destinacio;
+        this.cost = cost;
+        this.lliurat = false;
+    }
+    //Mètode
+    lliuramentFet(){
+        this.origen.incrementaDeute(this.cost);
+        this.lliurat = true;
+    }
+}*/
+
+//Exercicis que podeu fer vosaltres:
+
+//1. - Fer un llistat de les empreses que tenen deutes pendents.
+class Empresa{
+    constructor (nom, adreca){
+        // Propietats
+        this.nom = nom;
+        this.adreca = adreca;
+        this.deute = 0; // Quan creem l'empresa té dute 0.
+    }
+    //Mètode
+    incrementaDeute(n){
+        this.deute += n;
+    }
+    //Mètode
+    totPagat(){
+        yhis.deute = 0;
+    }
+}
+class Paquet{
+    constructor (origen, destinacio, cost){
+        //Propietats
+        this.origen = origen;
+        this.destinacio = destinacio;
+        this.cost = cost;
+        this.lliurat = false;
+    }
+    //Mètode
+    lliuramentFet(){
+        this.origen.incrementaDeute(this.cost);
+        this.lliurat = true;
+    }
+}
+
+// 2. - Fer un llistat dels paquets pendents de ser lliurats.
+// Ha de incloure: nom empresa origen i final, i cost que té l'enviament del paquet.
