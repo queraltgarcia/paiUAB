8794fd6cb72405a36418b6f4ee3bbf89ef620b7c
queraltgarcia <1706784@uab.cat>
2024-11-20 11:37:18 +0100

    Create exFuncions.js

---
+++ funcions/exFuncions.js
@@ -0,0 +1,26 @@
+//3.1. - Escriu un script que contingui una funció a la qual se li passa com a paràmetre un nombre enter i retorna com a resultat una cadena de text que indica si el número és parell o imparell. Mostra per pantalla el resultat retornat per la funció.
+/*function nombre (parametre){
+    let resultat = "";
+    if (parametre%2 === 0){
+        resultat = "El nombre és parell";
+    }else{
+        resultat = "El nombre és imparell";
+    }
+    return resultat;
+}*/ 
+
+//3.2. - Escriu un script que comprovi si un any donat és de traspàs. Un any és de traspàs si és múltiple de 4, excloent els que siguin múltiples de 100, però no els que siguin múltiples de 400. 
+/*function traspas (any){
+    let resultat = "";
+    if (any%4 === 0){
+        resultat = True;
+    }else if (any%100 === 0 && any%400 !== 0){
+        resultat = False;
+    }
+    return resultat;
+}*/
+
+//3.3. - Feu un script que inclogui una funció que, donat un enter N, retorni ∑ (1 / n2 ) per a 0 < n ≤ N.
+function suma (N){
+    for (let i=0; i)
+}
