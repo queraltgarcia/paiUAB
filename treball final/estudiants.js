0f367ee655eb7b412363ffc8171c36cf7abd4164
queraltgarcia <1706784@uab.cat>
2024-12-19 11:35:17 +0100

    Update estudiants.js

--- treball final/estudiants.js
+++ treball final/estudiants.js
@@ -4,13 +4,17 @@
 
 // Com que al document html no hi ha controladors d'esdeveniments, els haurem de crear aquí:
 
-
+document.getElementById("exer01").addEventListener("click", exercici01);
+document.getElementById("exer02").addEventListener("click", exercici02);
+document.getElementById("exer03").addEventListener("click", exercici03);
+document.getElementById("exer04").addEventListener("click", exercici04);
 
 // Teniu ja definides les funcions de cada exercici (menys del cinquè), només cal
 // que ompliu el codi necessari.
 
 function exercici01() {
 
+    
 }
 
 function exercici02() {
